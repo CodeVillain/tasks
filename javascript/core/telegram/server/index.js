0bccde774670be0b2447d4de32b99b24d8020c69
y.kabai <y.kabai@levi9.com>
2017-04-15 20:28:49 +0300

    Clean code

--- javascript/core/telegram/server/index.js
+++ javascript/core/telegram/server/index.js
@@ -1,27 +1,21 @@
 import Express from 'express';
 import http from 'http';
-import socketio from 'socket.io';
 import mongoose, { Types } from 'mongoose';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import Chat from './models/chat';
 import User from './models/user';
-import bodyParser from 'body-parser';
 
 import { createFixtures } from './config';
 
 const app = Express();
 const httpServer = http.Server(app);
-const io = socketio(httpServer);
 mongoose.connect('mongodb://127.0.0.1:27017/telegram');
 
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// io.on('connection', socket => {
-//   console.log('a user connected');
-// });
 
 app.get('/setup', createFixtures);
 
@@ -32,19 +26,15 @@ app.get('/chats', (req, res) => {
 app.get('/chats/:id', (req, res) => {
   Chat
     .findById(req.params.id)
-    .then(data => (
+    .then(chat => (
       Promise
-        .all(data.users.map(userId => User.findById(userId)))
+        .all(chat.users.map(userId => User.findById(userId)))
         .then((users) => {
-          data.users = users;
-          return res.send(data);
+          chat.users = users;
+          return res.send(chat);
         })
         .catch(err => console.log('Err', err))
     ));
-});
-
-app.post('/chats/:id', (req, res) => {
-  console.log('Post');
 });
 
 app.put('/chats/:id', (req, res) => {
